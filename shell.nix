69f83064855b38f7fa055118172e65703bd8b8b5
zedddie <zedddiezxc@gmail.com>
2025-12-15 21:00:54 +0100

    port consts & chip8 state struct

--- shell.nix
+++ shell.nix
@@ -1,2 +1,4 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell { nativeBuildInputs = with pkgs.buildPackages; [ cargo rustc ]; }
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [ SDL2 gcc cargo rustc ];
+}
