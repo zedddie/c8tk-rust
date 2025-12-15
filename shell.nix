07bf2f0dbf13b36f4cf25b59b08a9d93974930d0
zedddie <zedddiezxc@gmail.com>
2025-12-15 22:14:56 +0100

    build: add rust-analyzer

--- shell.nix
+++ shell.nix
@@ -1,4 +1,11 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [ SDL2 gcc cargo rustc ];
+  nativeBuildInputs = with pkgs.buildPackages; [
+    rust-analyzer
+    SDL2
+    gcc
+    rustfmt
+    cargo
+    rustc
+  ];
 }
